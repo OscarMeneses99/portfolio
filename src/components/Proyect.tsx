be705651d86c95dcbdaa5c041e43d7c370071cc1
Oscar Meneses <omenesess1800@alumno.ipn.mx>
2023-12-22 00:34:39 -0600

    Refactor Proyect component styling

--- src/components/Proyect.tsx
+++ src/components/Proyect.tsx
@@ -7,16 +7,16 @@ function Proyect({ title, description, image, link, code }:
     "shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]";
   return (
     <figure
-      className={`w-full flex flex-col md:flex-row md:row-span-2 justify-center items-center p-2 ${shadow}`}
+      className={`w-full flex flex-col md:flex-row md:row-span-2 justify-center items-center md:p-2 ${shadow}`}
     >
       <div className="flex flex-col justify-center items-center w-2/3 bg-black rounded md:p-5">
-        <h1 className="font-extrabold text-4xl mt-4 mb-4 text-center title">
+        <h1 className="font-extrabold text-4xl mt-6 mb-4 text-center title">
           {title}
         </h1>
 
         <p className="mt-1 text-center text-xl text-white/70">{description}</p>
       </div>
-      <div className="flex flex-col justify-center items-center w-full md:w-1/2 gap-5 mt-3">
+      <div className="flex flex-col justify-center items-center w-full md:w-1/2 gap-5 p-2 md:p-0 mt-3">
         <Image
           className="rounded-lg"
           src={image}
